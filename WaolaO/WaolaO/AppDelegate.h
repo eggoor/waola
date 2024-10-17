40e1494450c9ba3f8a14240201b2dd206f8962da
eggor <egggor@gmail.com>
2024-10-17 17:28:44 +0300

    WaolaO initial

---
+++ WaolaO/WaolaO/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WaolaO
+//
+//  Created by Igor Kriuchkov on 17.10.2024.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WaolaO/WaolaOTests/WaolaOTests.m
@@ -0,0 +1,36 @@
+//
+//  WaolaOTests.m
+//  WaolaOTests
+//
+//  Created by Igor Kriuchkov on 17.10.2024.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WaolaOTests : XCTestCase
+
+@end
+
+@implementation WaolaOTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
